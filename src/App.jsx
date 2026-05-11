f13fd6b9aa574df286219d21c3fe31cb7d2bb093
adafali-netizen <adafali@gmail.com>
2026-05-11 15:40:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ export default function App() {
             📞 Appeler {selectedLead.telephone}
           </a>
 
-          
+      <a    
 href={`https://wa.me/212${selectedLead.telephone.substring(1)}?text=Bonjour ${selectedLead.client_nom}`}
   target="_blank"
   rel="noreferrer"
